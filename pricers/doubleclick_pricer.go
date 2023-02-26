bc38583862c10252a16ebcbdfbd3d25b616b1e1b
Sergey Ponomarev <stokito@gmail.com>
2023-02-26 18:00:55 +0200

    pricers

--- pricers/doubleclick_pricer.go
+++ pricers/doubleclick_pricer.go
@@ -129,7 +129,7 @@ func (dc *DoubleClickPricer) DecryptRaw(encryptedPrice []byte, buf []byte) (uint
 
 	// pad = hmac(e_key, iv)
 	//pad := helpers.HmacSum(dc.encryptionKey, iv)[:8]
-	pad := binary.BigEndian.Uint64(HmacSum(dc.encryptionKey, iv)[:8])
+	pad := binary.BigEndian.Uint64(HmacSum2(dc.encryptionKeyRaw, iv)[:8])
 
 	// priceMicro = p <xor> pad
 	priceInMicros := pad ^ p
@@ -137,7 +137,7 @@ func (dc *DoubleClickPricer) DecryptRaw(encryptedPrice []byte, buf []byte) (uint
 	binary.BigEndian.PutUint64(priceMicro[:], priceInMicros)
 
 	// conf_sig = hmac(i_key, data || iv)
-	confirmationSignature := binary.BigEndian.Uint32(HmacSum(dc.integrityKey, append(priceMicro[:], iv[:]...))[:4])
+	confirmationSignature := binary.BigEndian.Uint32(HmacSum2(dc.integrityKeyRaw, append(priceMicro[:], iv[:]...))[:4])
 	//confirmationSignature := binary.BigEndian.Uint32(helpers.HmacSum(dc.integrityKey, priceMicro[:], iv)[:4])
 
 	// success = (conf_sig == sig)
